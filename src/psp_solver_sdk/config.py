457023775a4a9aa756d658b7a409fae115926a3c
Jonas Bork <59069012+jonas-bork@users.noreply.github.com>
2025-11-24 12:56:42 +0100

    Change environment variable names (#1)
    * Change environment variable names
    
    * Support python 3.12
    
    * Remove psp-auth stuff from conftest
    
    * Fixed ruff checks

--- src/psp_solver_sdk/config.py
+++ src/psp_solver_sdk/config.py
@@ -1,21 +1,23 @@
+import os
 from dataclasses import dataclass
 
 
 @dataclass
 class SolverConfig:
+    class CPU:
+        limit: int = os.getenv("CPU_LIMIT")
+
     class Queue:
         request_timeout: tuple[int, int] = (1, 5)
 
         class In:
-            Name: str = os.getenv("QUEUE_IN_NAME")
+            name: str = os.getenv("QUEUE_IN_NAME")
 
         class Out:
-            Name: str = os.getenv("QUEUE_OUT_NAME")
+            name: str = os.getenv("QUEUE_OUT_NAME")
 
         class Auth:
-            HOST: str = os.getenv(
-                "RABBITMQ_HOST", "rabbitmq.rabbit-mq.svc.cluster.local"
-            )
-            PORT: str = int(os.getenv("RABBITMQ_PORT", "5672"))
-            USER: str = os.getenv("RABBITMQ_USER", "guest")
-            PASSWORD: str = os.getenv("RABBITMQ_PASSWORD", "guest")
+            host: str = os.getenv("QUEUE_HOST")
+            port: int = int(os.getenv("QUEUE_PORT"))
+            user: str = os.getenv("QUEUE_USER")
+            password: str = os.getenv("QUEUE_PASSWORD")
--- src/psp_solver_sdk/core.py
+++ src/psp_solver_sdk/core.py
@@ -1,9 +1,11 @@
+import logging
 from .config import SolverConfig
-from dataclasses import dataclass
 
 logger = logging.getLogger(__name__)
 
 
-@dataclass
-class SolverApi:
+class SolverWrapper:
     config: SolverConfig
+
+    def __init__(self, config: SolverConfig):
+        self.config = config
